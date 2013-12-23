d98997248e436d1b664a24dfc975d8a0df7ba145
scott1028 <mic1028002@gmail.com>
2013-12-23 14:49:23 +0800

    modified:   py4j_work_with_java.py

--- py4j_work_with_java.py
+++ py4j_work_with_java.py
@@ -3,10 +3,13 @@
 
 from py4j.java_gateway import JavaGateway
 gateway = JavaGateway()							# connect to the JVM
+
+# 單純使用 Java 的方法
 random = gateway.jvm.java.util.Random()			# create a java.util.Random instance
 number1 = random.nextInt(10)					# call the Random.nextInt method
 number2 = random.nextInt(10)
 print(number1,number2)
 
-#addition_app = gateway.entry_point				# get the AdditionApplication instance
-#addition_app.addition(number1,number2)			# call the addition method
+# 調用 Java 副程式內的方法
+addition_app = gateway.entry_point				# get the AdditionApplication instance
+addition_app.addition(number1,number2)			# call the addition method
